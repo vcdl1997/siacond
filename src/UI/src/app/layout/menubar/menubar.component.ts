7ffed52c3073084e69bcf454569e42a7afbeb8e9
Vinicius Costa de Lima <vcdl1997@gmail.com>
2024-03-03 18:29:20 -0300

    refactor: melhorando css do componente menubar

--- src/UI/src/app/layout/menubar/menubar.component.ts
+++ src/UI/src/app/layout/menubar/menubar.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-menubar',
@@ -7,11 +9,33 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class MenubarComponent implements OnInit {
 
+  private readonly router: Router;
+
+  menu = environment.menu.employee;
+
   @Input() showOptionsMenu = false;
 
-  constructor() { }
+  constructor(
+    router :Router
+  ) {
+    this.router = router;
+  }
 
   ngOnInit(): void {
   }
 
+  toggleClass(event: any) {
+    let element = event.target;
+    const isLabel = element.tagName == "LABEL";
+
+    if(!isLabel){
+      element = element.closest("label");
+    }
+
+    element.classList.toggle('focus-menu-item');
+  }
+
+  navigate(url:string){
+    this.router.navigate([url]);
+  }
 }
