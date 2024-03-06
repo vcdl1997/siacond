ec0a092ff260ac72ee3334e4ad306d7e239fb06f
Vinicius Lima <55336407+vcdl1997@users.noreply.github.com>
2024-03-06 09:17:36 -0300

    Update PublicController.php

--- src/Application/Controllers/PublicController.php
+++ src/Application/Controllers/PublicController.php
@@ -19,7 +19,7 @@ class PublicController extends Controller{
 
     public function authentication()
     {
-        $retorno = $this->userService->authentication($this->data[self::RECEIVED]);
-        JSON::response($retorno, HttpStatusCode::OK);
+        $output = $this->userService->authentication($this->data[self::RECEIVED]);
+        JSON::response($output, HttpStatusCode::OK);
     }
 }
